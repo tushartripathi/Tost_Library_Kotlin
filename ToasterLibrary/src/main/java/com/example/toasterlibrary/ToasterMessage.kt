75bbd78c3ede7e5ffac0366cbdb96cdbeb0e0a5d
tushar <tushartripathi301997@gmail.com>
2021-03-15 12:50:59 +0530

    First comit

---
+++ ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.kt
@@ -0,0 +1,11 @@
+package com.example.toasterlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+
+class ToasterMessage {
+    fun s(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
